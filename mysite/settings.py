851fcab8f7a96e59ac9164685450f4b48b7b6300
Vaclav Jarolim <vasajarolim@seznam.cz>
2019-03-12 22:07:00 +0100

    oprava

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,4 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
